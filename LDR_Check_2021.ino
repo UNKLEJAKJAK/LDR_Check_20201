9adf05cd80e27eee10c3dddb3adef6a5d0aacd05
UNKLEJAKJAK <33608952+UNKLEJAKJAK@users.noreply.github.com>
2021-10-23 16:36:50 -0400

    Add files via upload

---
+++ LDR_Check_2021.ino
@@ -0,0 +1,33 @@
+//set pin numbers
+const int ledPin = 20; //the number of the LED pin
+const int ldrPin = A4; //the number of the LDR pin
+
+void setup() {
+  Serial.begin (1000);
+  pinMode (ledPin, OUTPUT); //initiate the LED pin as an output
+  pinMode (ldrPin, INPUT); //initiate the LDR pin as an input 
+}
+
+void loop() {
+
+  int ldrStatus = analogRead(ldrPin); //read the status of the LDR value
+
+  //check if the LDR status is <=300
+  //if it is, the LED is HIGH 
+
+    if (ldrStatus <=300) {
+      digitalWrite (ledPin, LOW);              //turn LED on
+      Serial.println ("LDR is DARK, LED is ON"); 
+      
+    }
+    else {
+      digitalWrite (ledPin, HIGH);       //turn LED off
+      Serial.println ("---------");
+    }
+
+}
+
+
+
+
+  
